a07cf78346a4cc0c9357d57079ea1b0c49176840
Andrew Young <foryoung89@gmail.com>
2019-08-03 18:41:12 +0800

    main: added routes for api
    added routes for totaltrips, averagespeed and averagefare
    using mock repo for development of api 
    switch to the bigquery repo later on

--- main.go
+++ main.go
@@ -1,13 +1,57 @@
 package main
 
 import (
+	"NYTaxiAnalytics/database"
 	"NYTaxiAnalytics/taxi"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	// for testing of functions
-	taxi.TotalTrips("2015-01-01", "2015-01-02")
+func setupRouter(s taxi.IService) *gin.Engine {
 
-	taxi.AverageSpeed("2015-01-01")
-	taxi.AverageFareLevel("2015-01-02", 16)
+	router := gin.New()
+
+	// Taxi api routes
+	router.GET("/total_trips", func(c *gin.Context) {
+		startdate := c.Query("start-date")
+		enddate := c.Query("end-date")
+
+		c.String(http.StatusOK, "Date %s %s", startdate, enddate)
+	})
+
+	router.GET("/average_speed", func(c *gin.Context) {
+		date := c.Query("date")
+
+		c.String(http.StatusOK, "Date %s", date)
+	})
+
+	router.GET("/average_fare_heatmap", func(c *gin.Context) {
+		date := c.Query("date")
+
+		c.String(http.StatusOK, "Date %s", date)
+
+	})
+
+	return router
+}
+
+func main() {
+	// Setup big query client
+	database.BigQueryClientSetup()
+
+	// Setup client, taxi repo, service
+	// Use mock repo for development
+	// TO DO: switch to bigquery client when done
+	client := database.TestClient{}
+	var r taxi.TaxiRepo = taxi.TaxiJsonRepo{
+		Client: client,
+	}
+	var s taxi.IService = taxi.Service{Repo: r}
+
+	// Setup router
+	router := setupRouter(s)
+
+	// Configure port
+	router.Run(":8080")
 }

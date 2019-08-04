ff363e9fcec2e9116f575194ec9f976317eb51bc
Andrew Young <foryoung89@gmail.com>
2019-08-04 21:24:15 +0800

    taxi: fixed bigquery client to wait for job to complete

--- database/bigQueryClient.go
+++ database/bigQueryClient.go
@@ -2,7 +2,9 @@ package database
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
+	"log"
+	"os"
 
 	"cloud.google.com/go/bigquery"
 )
@@ -16,49 +18,75 @@ type Configuration struct {
 
 var config = Configuration{}
 
+const dryRun = false
+
 // to do initialize in main method
 // Initializing Big Query handler, reading config file for
 // Google application credentials and project name
-/*
-func init() {
-	fmt.Println("Initializing Big Query handler")
 
-	file, err := os.Open("../config.json")
+func BigQueryClientSetup() {
+	log.Println("Initializing Big Query handler")
+
+	file, err := os.Open("config.json")
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 	decoder := json.NewDecoder(file)
 	err = decoder.Decode(&config)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 
 	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", config.ApplicationCredentialsPath)
-	proj := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
-	if proj == "" {
-		fmt.Println("GOOGLE_APPLICATION_CREDENTIALS environment must be set")
+
+	if config.ApplicationCredentialsPath == "" {
+		log.Println("GOOGLE_APPLICATION_CREDENTIALS environment must be set")
 		os.Exit(1)
 	}
 
-	fmt.Println(config.ApplicationCredentialsPath, config.ProjectName)
 }
-*/
+
 // Exceute a query using the big query client
 func (c BigQueryClient) Query(q string, parameters []bigquery.QueryParameter) (*bigquery.RowIterator, error) {
-	fmt.Println("running BigQueryClient")
+	log.Println("Running BigQueryClient")
+
+	if config.ProjectName == "" || config.ApplicationCredentialsPath == "" {
+		BigQueryClientSetup()
+	}
+
 	ctx := context.Background()
 
 	client, err := bigquery.NewClient(ctx, config.ProjectName)
 	if err != nil {
-		fmt.Println(err)
-		//	return nil, err
+		log.Println(err)
+		return nil, err
 	}
 
 	query := client.Query(q)
 	query.Parameters = parameters
-	query.DryRun = true
+	query.Location = "US"
+	query.DryRun = dryRun
 
-	fmt.Println(query.Q)
+	job, err := query.Run(ctx)
+	if err != nil {
+		return nil, err
+	}
 
-	return query.Read(ctx)
+	status, err := job.Wait(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if err := status.Err(); err != nil {
+		return nil, err
+	}
+
+	log.Println(job.Config())
+	log.Println(job.LastStatus())
+
+	res, err := job.Read(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
